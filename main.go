b73f712f99a995088c790a34741dfd320e693bf0
makometr <makometr@yandex.ru>
2021-07-01 01:19:54 +0300

    Task26-27 complete

--- main.go
+++ main.go
@@ -1,15 +1,4 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
 func main() {
-	a := []int{1, 3, 4, 5, 10, 20, 50, 25, 41, 3}
-	sort.Ints(a)
-	fmt.Println(a)
-	index := sort.SearchInts(sort.IntSlice(a), 50)
-
-	fmt.Println(a[index])
 }
---
+++ task26_27/task26.go
@@ -0,0 +1,22 @@
+package task26
+
+import (
+	"strings"
+)
+
+func Task26(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+func Task27(s string) string {
+	words := strings.Split(s, " ")
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+
+	return strings.Join(words, " ")
+}

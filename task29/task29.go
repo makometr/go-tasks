c2a9c78efff237500cc416ae32afbc469754b6ce
makometr <makometr@yandex.ru>
2021-07-01 03:06:03 +0300

    Task29 complete

---
+++ task29/task29.go
@@ -0,0 +1,22 @@
+package task29
+
+import "math/big"
+
+func Mul(x, y *big.Int) *big.Int {
+	return big.NewInt(0).Mul(x, y)
+}
+
+func Div(x, y *big.Int) *big.Int {
+	return big.NewInt(0).Div(x, y)
+}
+
+func Add(x, y *big.Int) *big.Int {
+	return big.NewInt(0).Add(x, y)
+}
+
+func Sub(x, y *big.Int) *big.Int {
+	return big.NewInt(0).Sub(x, y)
+}
+
+func Task29() {
+}

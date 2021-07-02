d67cdff7e0bfec4eb0f33df7e4b033361a5bdb93
makometr <makometr@yandex.ru>
2021-07-02 15:27:15 +0300

    Task refactored

--- task1/task1.go
+++ task1/task1.go
@@ -2,7 +2,29 @@ package task1
 
 import "fmt"
 
+type Action struct {
+	name string
+}
+
+func NewAction(name string) *Action {
+	return &Action{name: name}
+}
+
+func (a Action) DoWork() {
+	fmt.Println("Do", a.name)
+}
+
+type Human struct {
+	action Action
+}
+
+func (h Human) DoAction() {
+	fmt.Print("Human:")
+	h.action.DoWork()
+}
+
 // Task1 is task
 func Task1() {
-	fmt.Println("Task1")
+	man := Human{action: *NewAction("varit borsh")}
+	man.DoAction()
 }
